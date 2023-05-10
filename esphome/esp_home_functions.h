4c7c417694befc8f0d450851485218b2eaaf1af6
patfelst <patrickfelstead@gmail.com>
2023-05-10 23:14:17 +0930

    add new esphome include file

---
+++ esphome/esp_home_functions.h
@@ -0,0 +1,26 @@
+
+/* 
+** convert a number of seconds to a human readable C++ string of in the format:
+** "dd hh mm ss"
+*/
+std::string seconds_to_readable(int sec) {
+  int seconds = sec;
+  int minutes = 0;
+  int hours = 0;
+  int days = 0;
+  std::string txt = "";
+
+  days = seconds / (24 * 3600);
+  seconds = seconds % (24 * 3600);
+  hours = seconds / 3600;
+  seconds = seconds % 3600;
+  minutes = seconds /  60;
+  seconds = seconds % 60;
+
+  if (days) txt = (to_string(days) + "d "); else "";
+  if (hours) txt+= (to_string(hours) + "h "); else "";
+  if (minutes) txt+= (to_string(minutes) + "m "); else "";
+  txt+= to_string(seconds) + "s";
+  // ESP_LOGW("Sprinkler debug", "Time remaining is %s", txt.c_str());
+  return txt;
+}
